b6ff797df9c7b8ca48eaa4cd51e64bec2c64ee1b
Adriano <puttino@gmail.com>
2022-10-18 17:59:52 +0200

    Test

---
+++ TestExample/TestExampleApp.swift
@@ -0,0 +1,19 @@
+//
+//  TestExampleApp.swift
+//  TestExample
+//
+//  Created by Adriano Giordano on 18/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TestExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            //  Created by Adriano Giordano on 18/10/22.
+            //
+        }
+    }
+}
